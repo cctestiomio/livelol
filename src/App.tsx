0715af5b3829a862396ee02783b3fe5f9f6d5b21
unknown <andydangermarshall@gmail.com>
2026-02-06 11:35:48 -0600

    Thunderpick banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
 import './styles/global.css'
 import 'react-toastify/dist/ReactToastify.min.css';
 
-import {Match} from "./components/Match/Match";
-import {HashRouter, Switch, Route, Redirect} from "react-router-dom";
-import {Footer} from "./components/Footer/Footer";
-import {EventsSchedule} from "./components/Schedule/EventsSchedule";
-import {Navbar} from "./components/Navbar/Navbar";
+import { Match } from "./components/Match/Match";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import { Footer } from "./components/Footer/Footer";
+import { EventsSchedule } from "./components/Schedule/EventsSchedule";
+import { Navbar } from "./components/Navbar/Navbar";
 import { useTheme } from './theme/ThemeContext'
 import React from "react";
+import ThunderPickBanner from './thunderpick_960x140.png';
 
 function App() {
     const { theme } = useTheme();
 
     return (
         <HashRouter basename="/">
-            <div className="theme-container" style={{...theme as React.CSSProperties}}>
-                <Navbar/>
+            <div className="theme-container" style={{ ...theme as React.CSSProperties }}>
+                <Navbar />
                 <div className="container">
                     <Switch>
-                        <Route path="/" exact component={EventsSchedule}/>
-                        <Route path="/live/:gameid" component={Match}/>
-                        <Redirect to="/"/>
+                        <Route path="/" exact component={EventsSchedule} />
+                        <Route path="/live/:gameid" component={Match} />
+                        <Redirect to="/" />
                     </Switch>
+                    <a className='thunderPickBanner' href="https://go.thunder.partners/visit/?bta=37411&nci=5627&campaign=WELCOME" target="_top">
+                        <img src={ThunderPickBanner} width="100%" height="auto" loading="lazy" />
+                    </a>
                 </div>
-                <Footer/>
+                <Footer />
             </div>
         </HashRouter>
     );
